9c730e4777d78ec2e2f4c5ba189b2dc11a98e081
João Gabriel <joao.gbp@hotmail.com>
2022-08-23 14:35:21 -0300

    Create FeedbackLocalKeys.swift

---
+++ Sources/IOSFeedback/FeedbackLocalKeys.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by João Gabriel Borelli Padilha on 23/08/22.
+//
+
+import Foundation
+
+enum FeedbackLocalKeys: String {
+    case haptic = "settings_feedback_haptic"
+    case sound = "settings_feedback_sound"
+}

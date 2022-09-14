1808a38e795cd3e1a34ee7d96cd70c6d25f7d863
João Gabriel <joao.gbp@hotmail.com>
2022-09-14 10:13:26 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "IOSFeedback",
             dependencies: [],
-            resources: [.copy("Resources")]
+            resources: [.copy("Sources/IOSFeedback/Resources")]
         ),
         .testTarget(
             name: "IOSFeedbackTests",
